11df1dee7aa5d288cd5daa55c3e5bbef9ee79d3a
unknown <juanjo81156@gmail.com>
2023-05-20 19:02:46 -0300

    login

--- home/index.php
+++ home/index.php
@@ -4,9 +4,46 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="../css/estilo.css">
     <title>Document</title>
+        
+        <div id="contenedor">
+            
+            <div id="contenedorcentrado">
+                <div id="login">
+                    <form id="loginform">
+                        <label for="usuario">Usuario</label>
+                        <input id="user" type="text" name="user" placeholder="Usuario" required>
+                        
+                        <label for="password">Contraseña</label>
+                        <input id="password" type="password" placeholder="Contraseña" name="password" required>
+                        
+                        <button type="submit" title="Ingresar" name="Ingresar">Iniciar Sesion</button>
+                    </form>
+                    
+                </div>
+                <div id="derecho">
+                    <div class="titulo">
+                       Bienvenido a 
+                    
+                       <div>CRUD-PHP</div> 
+                    </div>
+                    
+                    <hr>
+                    <div class="pie-form">
+                        <a href="#">¿No recuerdas tu contraseña?</a>
+                        <a href="#">¿No tienes Cuenta? Registrate</a>
+                        <a> Registrarse</a>
+                        <hr>
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </body>
+</html>
+
 </head>
 <body>
-    
-</body>
-</html>
